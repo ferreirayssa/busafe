9b83b54e4ccde769e4adf0e6a6619bcf196d971d
xneesany <xneesan07@gmail.com>
2026-03-19 12:47:10 -0300

    Arquivos do busafe adicionados corretamente

---
+++ busafe/src/main/java/com/transcol/busafe/repository/PontoRotaRepository.java
@@ -0,0 +1,9 @@
+package com.transcol.busafe.repository;
+
+import com.transcol.busafe.model.PontoRota;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PontoRotaRepository extends JpaRepository<PontoRota, Long> {
+  List<PontoRota> findByRotaIdOrderByOrdemAsc(Long rotaId);
+}
---
+++ busafe/src/main/java/com/transcol/busafe/repository/RotaRepository.java
@@ -0,0 +1,19 @@
+package com.transcol.busafe.repository;
+
+import com.transcol.busafe.model.Rota;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RotaRepository extends JpaRepository<Rota, Long> {
+    Rota findFirstByLinhaTranscolOrderByIdAsc(String codigo);
+    Rota findFirstByLinhaTranscolAndSentidoIgnoreCaseOrderByIdAsc(String codigo, String sentido);
+    Rota findFirstByLinhaMunicipalOrderByIdAsc(String codigo);
+    Rota findFirstByLinhaMunicipalAndSentidoIgnoreCaseOrderByIdAsc(String codigo, String sentido);
+
+    boolean existsByLinhaTranscol(String codigo);
+    boolean existsByLinhaMunicipal(String codigo);
+
+    List<Rota> findAllByLinhaTranscolOrLinhaMunicipal(String transcol, String municipal);
+    List<Rota> findTop10DistinctByLinhaTranscolContainingIgnoreCaseOrLinhaMunicipalContainingIgnoreCase(String termoTranscol, String termoMunicipal);
+}
