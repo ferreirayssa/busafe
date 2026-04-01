f5b859b1125f31add61ea73f301e3a92345f72d0
Rayssa <xneesan07@gmail.com>
2026-04-01 11:30:20 -0300

    Details

--- src/main/java/com/transcol/busafe/repository/RotaRepository.java
+++ src/main/java/com/transcol/busafe/repository/RotaRepository.java
@@ -28,10 +28,21 @@ public interface RotaRepository extends MongoRepository<Rota, String> {
     // O MongoDB lida muito bem com buscas 'Containing' e 'IgnoreCase'
     List<Rota> findTop10DistinctByLinhaTranscolContainingIgnoreCaseOrLinhaMunicipalContainingIgnoreCase(String termoTranscol, String termoMunicipal);
 
+<<<<<<< rayssaf
+    @Query(value = "{ 'linhaTranscol': ?0 }", fields = "{ 'pontos': 1, '_id': 0 }")
+    Rota findPointsByLinhaTranscol(String codigo);
+
+    @Query(value = "{ '$or': [ " +
+                "{ 'linhaTranscol': { '$regex': ?0, '$options': 'i' } }, " +
+                "{ 'linhaMunicipal': { '$regex': ?1, '$options': 'i' } } " +
+                "] }", 
+        fields = "{ 'pontos': 0 }")
+=======
     @Query(value = "{ 'linha_transcol': ?0 }", fields = "{ 'pontoRota': 1, '_id': 0 }")
     Rota findPointsByLinhaTranscol(String codigo);
 
     @Query(value = "{ $or: [ { 'linha_transcol': { $regex: ?0, $options: 'i' } }, { 'linha_municipal': { $regex: ?1, $options: 'i' } } ] }", 
            fields = "{ 'pontoRota': 0 }")
+>>>>>>> main
     List<Rota> findSuggestionsWithoutPoints(String termoTranscol, String termoMunicipal);
 }
